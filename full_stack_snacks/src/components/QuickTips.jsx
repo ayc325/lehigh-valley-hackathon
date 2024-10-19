1ee09c609eb1290f6ac7e2631604af556aea352a
SophiaCXT <soc422@lehigh.edu>
2024-10-19 13:35:05 -0400

    progress update

--- full_stack_snacks/src/components/QuickTips.jsx
+++ full_stack_snacks/src/components/QuickTips.jsx
@@ -1,8 +1,74 @@
 import React from "react"
+import notionpic from "../assets/notionpic.jpg"
 const QuickTips = () =>{
     return(
         <div className = "tips">
             <h2>Quick Tips</h2>
+            <p>
+                Did you know that according to the Princeton Review, Lehigh is ranked as the second most studious school, above MIT! 
+                To relieve some of the stress from studying, here are some quick tips to help you manage stress, improve your mental 
+                wellness, and balance schoolwork with self-care. Perfect for when you need fast and simple solutions.
+            </p>
+            <h3>
+                Stress-Reducing Activities on Campus
+            </h3>
+            <p>
+                <b>Lehigh BikeShare</b>
+                : Lehigh's Bikeshare program offers a convenient, eco-friendly way to get around campus, 
+                and can be a fun recreational activity with friends!
+                <br />
+                How to rent a bike: <a href = "https://auxiliaryservices.lehigh.edu/more-transportation-options/bikeshare"> BikeShare | Auxiliary Services</a>
+            </p>
+            <p>
+                <b>Lehigh Involvement Connection (LINC)</b>
+                : LINC is a virtual campus center and is the primary location to find out what events are happening on Lehigh's campus! 
+                Take a break from studying to socialize with other students and get involved on campus!
+                <br />
+                Explore LINC: <a href = "https://lehigh.campuslabs.com/engage/"> Lehigh Involvement Center | Student Affairs</a>
+            </p>
+            <p>
+                <b>Lehigh After Dark (LAD)</b>
+                : LAD helps create a vibrant campus nightlife by sponsoring a variety of social options that do not focus on alcohol.
+                <br />
+                For more information: <a href = "https://studentaffairs.lehigh.edu/lehigh-after-dark-lad">Lehigh After Dark | Student Affairs</a>
+            </p>
+            
+            <h3>Apps</h3>
+            <p>There are collection of digital interfaces made to assist student with studying, staying organized, and connecting with others. 
+                Here are a variety of apps to enhance your college experience and support mental wellness.
+            </p>
+            <p>
+                <b>Google Calendar</b>
+                : Google Calendar helps you organize your classes, meetings and assignments all in one place. Set reminders, schedule study sessions, 
+                and manage your academic life with ease.
+            </p>
+            <p>
+                <b>Notion</b>
+                : Notion is a versitile tool for note-taking, assignment tracking, and journaling. 
+                Notion is useful for both academic and personal use, with customizable templates to fit your needs.
+                <br />
+                <a href = "https://www.bing.com/ck/a?!&&p=be649260b6a656b6b6897cdeaee324a8fe2e4d38c61f7b914f670bde5a963cacJmltdHM9MTcyOTI5NjAwMA&ptn=3&ver=2&hsh=4&fclid=0942a05a-4f87-6ce9-04ec-b0c44e376d62&psq=notion+dowload&u=a1aHR0cHM6Ly93d3cubm90aW9uLnNvLw&ntb=1">
+                    <img src = {notionpic}></img>
+                </a>
+            </p>
+            <p>
+                <b>Pomodoro Timer</b>
+                : The Pomodoro Timer app encourages undistracted studying using the Pomodoro Technique -- 25 mintues of studying separated by 5 minutes of break.
+                <br />
+                <a href = "https://www.bing.com/ck/a?!&&p=6c6bf2a3e1375cf93a8a242ecfa45cd246b626329cdac7fbe5aadc9ede5ce776JmltdHM9MTcyOTI5NjAwMA&ptn=3&ver=2&hsh=4&fclid=0942a05a-4f87-6ce9-04ec-b0c44e376d62&psq=pomodoro+timer+app&u=a1aHR0cHM6Ly9wbGF5Lmdvb2dsZS5jb20vc3RvcmUvYXBwcy9kZXRhaWxzP2lkPWNvbS5wb21vZHJvbmUuYXBwJmhsPWVuLVVT&ntb=1" Download Pomodoro Timer on Google Play></a>
+            </p>
+            <p>
+                <b>Forest</b>
+                : The Forest app rewards your focus by letting you grow a virtual tree! The longer you stay off your phone, 
+                the bigger your tree grows. Grow a collection of unique tree species and you improve your study habits.
+                
+                <br />
+            </p>
+            <p>
+                <b>Stompers</b>
+
+                <br />
+            </p>
         </div>
     )
 }
