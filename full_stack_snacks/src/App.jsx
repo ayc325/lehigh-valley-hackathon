e6405dd35a77a9be7bd1b3b46549d934447d5cf8
EstherFrank <esf228@lehigh.edu>
2024-10-20 03:35:02 -0400

    need to fix formatting of home screen

--- full_stack_snacks/src/App.jsx
+++ full_stack_snacks/src/App.jsx
@@ -8,9 +8,8 @@ import './App.css';
 
 function App() {
   return (
-    <Router>  
+    <Router>
       <div className="grid-container">
-        
         <div className="Welcome1">
           <h1>Mindful Hawks</h1>
         </div>
@@ -20,58 +19,56 @@ function App() {
           <Link to="/moodtracker">Mood Tracker</Link>
           <Link to="/quicktips">Quick Tips</Link>
           <Link to="/resources">Resources</Link>
+          <Link to="/">Home</Link>
         </div>
 
-        <main className = "body1">
+        <main className="body1">
           <Routes>
+            <Route path="/" element={<Home />} />
             <Route path="/chatbot" element={<ChatBot />} />
             <Route path="/moodtracker" element={<MoodTracker />} />
             <Route path="/quicktips" element={<QuickTips />} />
             <Route path="/resources" element={<Resources />} />
-            <Route path="/" element={<h2>Welcome! Please select a section.</h2>} />
             <Route path="*" element={<h2>404 - Page Not Found</h2>} />
           </Routes>
         </main>
-
-
-        <div className="playlist1">
-          <iframe
-            style={{ borderRadius: '12px' }} 
-            src="https://open.spotify.com/embed/playlist/6dQz1ZnwnluFiUhBOMrJQC?utm_source=generator" 
-            width="100%" 
-            height="370" 
-            frameBorder="0" 
-            allowFullScreen 
-            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
-            loading="lazy">
-          </iframe>
-        </div>
-        
-        <div className="fact">
-          <div className="funfact1">
-            <p>Did you know...?</p>
-          </div>
-          <div className="revealedFact">
-            <p>Lehigh is the 2nd most studious college in the US!</p>
-          </div>
-        </div>
-
-
-        
-
-        {/* <div className="body1">
-          <p>body goes here</p>
-        </div> */}
-
-        <div className="moodInsights">
-          <Calendar1 />
-        </div>
 
         <div className="footer1">
           <p>footer goes here</p>
         </div>
       </div>
     </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome! Please select a section.</h2>
+      <div className="playlist1">
+        <iframe
+          style={{ borderRadius: '12px' }} 
+          src="https://open.spotify.com/embed/playlist/6dQz1ZnwnluFiUhBOMrJQC?utm_source=generator" 
+          width="100%" 
+          height="370" 
+          frameBorder="0" 
+          allowFullScreen 
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+          loading="lazy">
+        </iframe>
+      </div>
+      <div className="fact">
+        <div className="funfact1">
+          <p>Did you know...?</p>
+        </div>
+        <div className="revealedFact">
+          <p>Lehigh is the 2nd most studious college in the US!</p>
+        </div>
+      </div>
+      <div className="moodInsights">
+        <Calendar1 />
+      </div>
+    </div>
   );
 }
 
